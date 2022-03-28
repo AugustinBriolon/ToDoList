fd066a6d46ed996e1ee2cdc69f966fdf56595d4b
Augustin <augustin.briolon@gmail.com>
2022-03-28 08:04:17 +0200

    :sparkles: check todo and filter

--- assets/app.js
+++ assets/app.js
@@ -2,11 +2,14 @@
 const toDoInput = document.querySelector('.todo-input');
 const toDoBtn = document.querySelector('.todo-btn');
 const toDoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo')
 
 
 //Events Listeners
-toDoBtn.addEventListener('click', addToDo);
-toDoList.addEventListener('click', deleteToDo);
+document.addEventListener('DOMContentLoaded', getToDos)
+toDoBtn.addEventListener('click', addToDo)
+toDoList.addEventListener('click', deleteToDo)
+filterOption.addEventListener('change', filterToDo)
 
 
 //Functions
@@ -20,6 +23,8 @@ function addToDo(event) {
     newToDo.innerText = toDoInput.value
     newToDo.classList.add('todo-item')
     toDoDiv.appendChild(newToDo)
+    //Add todo to LocalStorage
+    saveLocalToDos(toDoInput.value)
     //Create a button to check the todo
     const checkBtn = document.createElement('button')
     checkBtn.innerHTML = '<i class="fas fa-check"></i>'
@@ -43,6 +48,7 @@ function deleteToDo(event) {
         const todo = item.parentElement;
         //Animation
         todo.classList.add('fall')
+        removeLocalToDos(todo)
         todo.addEventListener('transitionend', function () {
             todo.remove()
         })
@@ -54,4 +60,88 @@ function deleteToDo(event) {
         const todo = item.parentElement;
         todo.classList.toggle('completed')
     }
+}
+
+function filterToDo(e) {
+    const todos = toDoList.childNodes;
+    todos.forEach(function (todo) {
+        switch (e.target.value) {
+            case 'all':
+                todo.style.display = 'flex'
+                break;
+            case 'completed':
+                if (todo.classList.contains('completed')) {
+                    todo.style.display = 'flex'
+                } else {
+                    todo.style.display = 'none'
+                }
+                break
+            case 'uncompleted':
+                if (!todo.classList.contains('completed')) {
+                    todo.style.display = 'flex'
+                } else {
+                    todo.style.display = 'none'
+                }
+                break
+        }
+    })
+}
+
+function saveLocalToDos(todo) {
+    let todos
+    if (localStorage.getItem('todos') === null) {
+        todos = []
+    } else {
+        todos = JSON.parse(localStorage.getItem('todos'))
+    }
+
+    todos.push(todo)
+    localStorage.setItem('todos', JSON.stringify(todos))
+}
+
+function getToDos() {
+    let todos
+    if (localStorage.getItem('todos') === null) {
+        todos = []
+    } else {
+        todos = JSON.parse(localStorage.getItem('todos'))
+    }
+    todos.forEach(function (todo) {
+        //Create a div to hold the todo
+        const toDoDiv = document.createElement('div')
+        toDoDiv.classList.add('todo')
+
+        //Create a li to hold the todo
+        const newToDo = document.createElement('li')
+        newToDo.innerText = todo
+        newToDo.classList.add('todo-item')
+        toDoDiv.appendChild(newToDo)
+
+        //Create a button to check the todo
+        const checkBtn = document.createElement('button')
+        checkBtn.innerHTML = '<i class="fas fa-check"></i>'
+        checkBtn.classList.add('check-btn')
+        toDoDiv.appendChild(checkBtn)
+
+        //Create a button to delete the todo
+        const deleteBtn = document.createElement('button')
+        deleteBtn.innerHTML = '<i class="fas fa-trash"></i>'
+        deleteBtn.classList.add('trash-btn')
+        toDoDiv.appendChild(deleteBtn)
+
+        //Append todo to the list
+        toDoList.appendChild(toDoDiv)
+    })
+}
+
+function removeLocalToDos(todo) {
+    let todos
+    if (localStorage.getItem('todos') === null) {
+        todos = []
+    } else {
+        todos = JSON.parse(localStorage.getItem('todos'))
+    }
+    const todoIndex = todos.indexOf(todo)
+    todos.splice(todoIndex, 1)
+    localStorage.setItem('todos', JSON.stringify(todos))
 }
